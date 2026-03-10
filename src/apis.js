71a1986661d86bde5d593c2000a202abbeeefe4c
Vishalsr5002 <vishalrajasekaran123@gmail.com>
2026-03-10 18:22:03 +0530

    Add files via upload

--- src/apis.js
+++ src/apis.js
@@ -1,36 +1,41 @@
 export const APIs = [
   {
-    id: "display-dev",
-    category: "displayAnimations",
-    name: "Displaying Animations 1",
-    image: "spares.jpg",
-    url: "https://dev.motovisuals.com/thirdpartyapi/#!/viewAnimation/7011?show_menu=0&is_interactive=1&show_left_sidebar=0&show_description=0&video_only=0&auto_play=0",
+    id: "display1",
+    name: "Displaying Animation 1",
+    image: "/spares.jpg",
+
+    baseUrl: "https://motovisuals.com/thirdpartyapi/#!/thirdPartyLogin",
+
     params: {
-      Login: "motovisuals",
-      Password: "motovisuals",
-      show_menu: 0,
-      is_interactive: 1,
+      api_key: "tg2zw99gwqb5",
+      api_name: "animation_page",
+      part_id: 7011,
+      is_interactive: 0,
       show_left_sidebar: 0,
+      show_menu: 0,
       show_description: 0,
       video_only: 0,
       auto_play: 0
     }
   },
+
   {
-    id: "display-prod",
-    category: "displayAnimations",
-    name: "Displaying Animations 2",
-    image: "spares.jpg",
-    url: "https://motovisuals.com/thirdpartyapi/#!/viewAnimation/7011?show_menu=0&is_interactive=0&show_left_sidebar=0&show_description=0&video_only=0&auto_play=0",
+    id: "display2",
+    name: "Displaying Animation 2",
+    image: "/spares.jpg",
+
+    baseUrl: "https://motovisuals.com/thirdpartyapi/#!/thirdPartyLogin",
+
     params: {
-      Login: "motovisuals",
-      Password: "motovisuals",  
-      "Show menu": 0,
-      is_interactive: 0,
+      api_key: "tg2zw99gwqb5",
+      api_name: "animation_page",
+      part_id: 7011,
+      is_interactive: 1,
       show_left_sidebar: 0,
+      show_menu: 0,
       show_description: 0,
-      video_only: 0,
-      auto_play: 0
+      video_only: 1,
+      auto_play: 1
     }
   }
 ];
