7908d77bdafedabe44f1018ccbb70865dee6067d
Vishalsr5002 <vishalrajasekaran123@gmail.com>
2026-05-06 18:21:49 +0530

    Add user and track routes to server

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
 import pdfRoutes from "./routes/pdf.js";
+import userRoutes from "./routes/user.js";
+import trackRoutes from "./routes/track.js"; 
 const app = express();
 const PORT = 5000;
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.json());
+// Routes
 app.use("/api", pdfRoutes);
+app.use("/api", userRoutes);
+app.use("/api", trackRoutes); 
 app.get("/", (req, res) => {
   res.send("Server is working");
 });
