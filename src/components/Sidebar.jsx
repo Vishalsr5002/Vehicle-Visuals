972cbddb406e00053ae0f570ff8899c617a45901
Vishalsr5002 <vishalrajasekaran123@gmail.com>
2026-06-02 10:46:14 +0530

    Refactor Sidebar component and add new menu items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,9 +5,10 @@ import {
   RefreshCw,
   Mail,
   ChevronDown,
-  ChevronRight
+  ChevronRight,
+  SquarePlus,
+  View
 } from "lucide-react";
-
 export const Sidebar = ({ onSelectOption }) => {
   const [openMenu, setOpenMenu] = useState(null);
   const toggleMenu = (menu) => {
@@ -16,23 +17,6 @@ export const Sidebar = ({ onSelectOption }) => {
   return (
     <div className="sidebar">
       <h3>GET YOUR API KEY</h3>
-      <div className="menu-item" onClick={() => toggleMenu("api")}>
-        <Key size={18} />
-        <span>API key details</span>
-        {openMenu === "api" ? (
-          <ChevronDown size={16} />
-        ) : (
-          <ChevronRight size={16} />
-        )}
-      </div>
-
-      {openMenu === "api" && (
-        <ul className="submenu">
-          <li onClick={() => onSelectOption("api")}>
-            API key details
-          </li>
-        </ul>
-      )}
       <h5>ANIMATIONS</h5>
       <div className="menu-item" onClick={() => toggleMenu("animations")}>
         <Film size={18} />
@@ -45,46 +29,38 @@ export const Sidebar = ({ onSelectOption }) => {
       </div>
       {openMenu === "animations" && (
         <ul className="submenu">
-          
           <li onClick={() => onSelectOption("catalog")}>
             Animation Catalog
           </li>
-          
           <li
             style={{ cursor: "pointer" }}
             onClick={() => onSelectOption("display")}>
             Displaying Animations
           </li>
-
           <li onClick={() => onSelectOption("links")}>
             Generating Links for sending
           </li>
-          
           <li onClick={() => onSelectOption("videoDetails")}>
             Get Video Details
           </li>
-
           <li onClick={() => onSelectOption("search")}>
             Search Animations
           </li>
-
           <li onClick={() => onSelectOption("share")}>
             Get Animation Share Link
           </li>
-
           <li onClick={() => onSelectOption("update")}>
             Update Animation Link
           </li>
-
           <li onClick={() => onSelectOption("usage")}>
             Animation Link Usage
           </li>
-
           <li onClick={() => onSelectOption("viewed")}>
             Animations Viewed Report
           </li>
         </ul>
       )}
+  
       <h5>LOOPED ANIMATIONS</h5>
       <div className="menu-item" onClick={() => toggleMenu("looped")}>
         <RefreshCw size={18} />
@@ -95,13 +71,11 @@ export const Sidebar = ({ onSelectOption }) => {
           <ChevronRight size={16} />
         )}
       </div>
-
       {openMenu === "looped" && (
         <ul className="submenu">
           <li onClick={() => onSelectOption("generateLoop")}>
             Generate Looped Animations API Link
           </li>
-
           <li onClick={() => onSelectOption("autoLogin")}>
             Auto Login to Looped Animation Link
           </li>
@@ -113,11 +87,10 @@ export const Sidebar = ({ onSelectOption }) => {
         <span>Email An Animation</span>
         {openMenu === "email" ? (
           <ChevronDown size={16} />
-        ) : (
+        ):(
           <ChevronRight size={16} />
         )}
       </div>
-
       {openMenu === "email" && (
         <ul className="submenu">
           <li onClick={() => onSelectOption("emailAnimation")}>
@@ -125,9 +98,9 @@ export const Sidebar = ({ onSelectOption }) => {
           </li>
         </ul>
       )}
-      <h5>MISC APIs</h5>
+      <h5>ADDITIONAL APIs</h5>
       <div className="menu-item" onClick={() => toggleMenu("additionalAPI")}>
-        <square-plus size={18} />
+        <SquarePlus size={18} />
         <span>Additional APIs</span>
         {openMenu === "additionalAPI" ? (
           <ChevronDown size={16} />
@@ -137,42 +110,51 @@ export const Sidebar = ({ onSelectOption }) => {
       </div>
       {openMenu === "additionalAPI" && (
         <ul className="submenu">
-          
           <li onClick={() => onSelectOption("auto")}>
             Auto-Login
           </li>
-          
           <li
             style={{ cursor: "pointer" }}
             onClick={() => onSelectOption("lookup")}>
             Website Lookup by API Key
           </li>
-
           <li onClick={() => onSelectOption("prompt")}>
             User Upgrade Prompt
           </li>
-          
           <li onClick={() => onSelectOption("lite")}>
             Upgrade Lite User
           </li>
-
           <li onClick={() => onSelectOption("pdf")}>
             Animation PDF
           </li>
-
           <li onClick={() => onSelectOption("get")}>
             Get API Key
           </li>
-
           <li onClick={() => onSelectOption("preference")}>
             Get User Preference
           </li>
-
           <li onClick={() => onSelectOption("details")}>
             Get User Details
           </li>
         </ul>
       )}
+      <h5>VIEWING ANIMATIONS</h5>
+       <div className="menu-item" onClick={() => toggleMenu("viewing")}>
+        <View size={18} />
+        <span>Viewing Animations</span>
+        {openMenu === "viewing" ? (
+          <ChevronDown size={16} />
+        ) : (
+          <ChevronRight size={16} />
+        )}
+      </div>
+      {openMenu === "viewing" && (
+        <ul className="submenu">
+          <li onClick={() => onSelectOption("viewing")}>
+            Viewing Animations Page
+          </li>
+        </ul>
+       )}
     </div>
   );
 };
